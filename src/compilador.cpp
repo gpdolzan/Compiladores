d5d6394bd0e25ae11632b83ad4cde6eaf1e8ec75
Casadororo <51424037+Casadororo@users.noreply.github.com>
2024-05-12 14:12:49 -0300

    Tabela de Simbolos para Classe.
    Modificado as funcoes que operam sobre a Tabela de Simbolos para serem funcoes da classe.
    
    Same...

--- src/compilador.cpp
+++ src/compilador.cpp
@@ -8,15 +8,16 @@
 std::string simbolo_flex;
 flex_simbolos relacao;
 char token[TAM_SIMBOLO];
-tabela_simbolos *tabela_simb;
+TabelaSimbolos *tabela_simb;
 
 std::ofstream mepa_stream;
 std::string meu_token;
-int nl = 1, num_same_type_vars = 0, num_total_vars = 0, top_desloc = 0;
+int nl = 1, num_same_type_vars = 0, num_total_vars = 0, top_desloc = 0,
+    instruction_count = 0;
 int D[4] = {0, 0, 0, 0};
 std::stack<int> stack_block_var_count;
 
-void geraCodigo(const std::string& comando, const std::string& rot, int number,
+void geraCodigo(const std::string &comando, const std::string &rot, int number,
                 int nivel_lexico) {
   if (rot == "") {
     if (number == -1)
@@ -25,16 +26,19 @@ void geraCodigo(const std::string& comando, const std::string& rot, int number,
       if (nivel_lexico == -1)
         mepa_stream << "\t" << comando << " " << number << std::endl;
       else
-        mepa_stream << "\t" << comando << " " << nivel_lexico << ", " << number << std::endl;
+        mepa_stream << "\t" << comando << " " << nivel_lexico << ", " << number
+                    << std::endl;
     }
   } else {
     mepa_stream << rot << ": " << comando;
   }
 
+  instruction_count++;
+
   mepa_stream.flush();
 }
 
-int imprimeErro(const std::string& erro) {
+int imprimeErro(const std::string &erro) {
   // fprintf(stderr, "Erro na linha %d - %s\n", nl, erro);
   // exit(-1);
   return 1;
@@ -42,44 +46,40 @@ int imprimeErro(const std::string& erro) {
 
 void iniciaCompilador() {
   mepa_stream.open("mepa.txt", std::ofstream::out);
-  tabela_simb = iniciar_tabela();
+  tabela_simb = new TabelaSimbolos;
 }
 
 void desligaCompilador() {}
 
-void insereSimbolo(Simbolo *simb) { push(tabela_simb, simb); }
+void insereSimbolo(Simbolo *simb) { tabela_simb->push(simb); }
 
-Simbolo *buscaSimbolo(const std::string& simb) {
-  t_node *current = tabela_simb->top;
+Simbolo *buscaSimbolo(const std::string &simb) {
+  Simbolo *sim = tabela_simb->busca_simbolo(simb);
 
-  while (current != NULL) {
-    if (current->simbolo->identificador == simb) {
-      return current->simbolo;
-    }
-    current = current->prev;
-  }
+  if (sim == nullptr)
+    error("Variável não declarada: " + simbolo_flex);
 
-  error("Variável não declarada: " + simbolo_flex);
-
-  return nullptr;
+  return sim;
 }
 
 void removeSimbolo(int quant) {
   for (int i = 0; i < quant; i++) {
-    pop(tabela_simb);
+    delete tabela_simb->pop();
   }
 }
 
-tipo_variavel aplicarOperacao(const std::string& op, tipo_variavel necessary_type, tipo_variavel var1, tipo_variavel var2) {
+tipo_variavel aplicarOperacao(const std::string &op,
+                              tipo_variavel necessary_type, tipo_variavel var1,
+                              tipo_variavel var2) {
   if (!((var1 == var2) && (var1 == t_int)))
-      error("type mismatch");
-  
+    error("type mismatch");
+
   geraCodigo(op);
 
   return necessary_type;
 }
 
-void colocaTipoEmSimbolos(const std::string& tipo, int quantidade) {
+void colocaTipoEmSimbolos(const std::string &tipo, int quantidade) {
   tipo_variavel tipo_v;
 
   if (tipo == "boolean")
@@ -89,13 +89,13 @@ void colocaTipoEmSimbolos(const std::string& tipo, int quantidade) {
   else
     tipo_v = t_undefined;
 
-  coloca_tipo_em_simbolos(tabela_simb, tipo_v, quantidade);
+  tabela_simb->coloca_tipo_em_simbolos(tipo_v, quantidade);
 }
 
-void visualizaTabela() { print_tabela(tabela_simb); }
+void visualizaTabela() { tabela_simb->print_tabela(); }
 
 // espelho de bison::Parse::error
-void error(const std::string& msg) {
-    std::cerr << msg << " at line " << nl << '\n';
-    exit(0);
+void error(const std::string &msg) {
+  std::cerr << msg << " at line " << nl << '\n';
+  exit(0);
 }
--- src/utils/tabela_simbolos.cpp
+++ src/utils/tabela_simbolos.cpp
@@ -1,16 +1,6 @@
 #include "../../includes/utils/tabela_simbolos.hpp"
 #include <iostream>
 
-tabela_simbolos *iniciar_tabela() {
-  tabela_simbolos *nova_tabela = (tabela_simbolos *)malloc(sizeof(tabela_simbolos));
-  if (nova_tabela != nullptr) {
-    nova_tabela->top = nullptr;
-    nova_tabela->bottom = nullptr;
-  }
-
-  return nova_tabela;
-}
-
 t_node *criar_nodo(Simbolo *simbolo, t_node *prev) {
   t_node *new_node = new t_node();
   if (new_node != nullptr) {
@@ -26,72 +16,72 @@ t_node *criar_nodo(Simbolo *simbolo, t_node *prev) {
   return new_node;
 }
 
-void push(tabela_simbolos *tabela, Simbolo *simbolo) {
-  if (tabela != nullptr) {
-    t_node *novo_nodo = criar_nodo(simbolo, tabela->top);
+void TabelaSimbolos::push(Simbolo *simbolo) {
+  t_node *novo_nodo = criar_nodo(simbolo, this->top);
 
-    if (novo_nodo != nullptr) {
-      if (tabela->top == nullptr) { // Caso a tabela esteja inicialmente vazia
-        tabela->bottom = novo_nodo; // O primeiro elemento é tanto o topo quanto o fundo
-      }
-      tabela->top = novo_nodo; // Atualiza o topo para o novo nodo
+  if (novo_nodo != nullptr) {
+    if (this->top == nullptr) { // Caso a tabela esteja inicialmente vazia
+      this->bottom =
+          novo_nodo; // O primeiro elemento é tanto o topo quanto o fundo
     }
+    this->top = novo_nodo; // Atualiza o topo para o novo nodo
   }
 }
 
-void coloca_tipo_em_simbolos(tabela_simbolos *tabela, tipo_variavel tipo, int quantidade) {
-    if (tabela == nullptr || tabela->top == nullptr) {
-        std::cout << "A tabela esta vazia ou não inicializada.\n";
-        return;
-    }
+void TabelaSimbolos::coloca_tipo_em_simbolos(tipo_variavel tipo,
+                                             int quantidade) {
+  if (this->is_stack_empty()) {
+    std::cerr << "A tabela esta vazia\n";
+    return;
+  }
 
-    t_node *current = tabela->top;
-    int count = 0;
+  t_node *current = this->top;
+  int count = 0;
 
-    while (current != nullptr && count < quantidade) {
-        current->simbolo->tipo_v = tipo;
-        current = current->prev;
-        count++;
-    }
+  while (current != nullptr && count < quantidade) {
+    current->simbolo->tipo_v = tipo;
+    current = current->prev;
+    count++;
+  }
 
-    if (count < quantidade) {
-        printf("Aviso: apenas %d símbolos foram modificados, pois a tabela contém menos símbolos que a quantidade especificada.\n", count);
-    }
+  if (count < quantidade) {
+    printf("Aviso: apenas %d símbolos foram modificados, pois a tabela contém "
+           "menos símbolos que a quantidade especificada.\n",
+           count);
+  }
 }
 
-int is_stack_empty(tabela_simbolos *tabela) {
-  return (tabela == nullptr || tabela->top == nullptr);
-}
+int TabelaSimbolos::is_stack_empty() { return this->top == nullptr; }
 
-Simbolo *busca_simbolo(tabela_simbolos *tabela, std::string simb)
-{
-  if (is_stack_empty(tabela)) {
+Simbolo *TabelaSimbolos::busca_simbolo(const std::string &simb) {
+  if (this->is_stack_empty()) {
     return nullptr;
   }
 
-  t_node *current = tabela->top;
+  t_node *current = this->top;
   while (current != nullptr) {
-    if(current->simbolo->identificador == simb)
+    if (current->simbolo->identificador == simb)
       return current->simbolo;
 
     current = current->prev; // Mude para 'prev' para ir do topo ao fundo
   }
+
   return nullptr;
 }
 
-Simbolo *pop(tabela_simbolos *tabela) {
-  if (is_stack_empty(tabela)) {
+Simbolo *TabelaSimbolos::pop() {
+  if (this->is_stack_empty()) {
     return nullptr;
   }
 
-  t_node *top_node = tabela->top;
+  t_node *top_node = this->top;
   Simbolo *simbolo = top_node->simbolo;
-  
-  tabela->top = top_node->prev;
+
+  this->top = top_node->prev;
   free(top_node);
 
-  if (tabela->top == nullptr) {
-    tabela->bottom = nullptr;
+  if (this->top == nullptr) {
+    this->bottom = nullptr;
   }
 
   return simbolo;
@@ -99,38 +89,41 @@ Simbolo *pop(tabela_simbolos *tabela) {
 
 // Function to print a symbol
 void print_simbolo(Simbolo *s) {
-    if (s != nullptr) {
-        printf("Simbolo: %s, Tipo: %d, Nivel Lexico: %d, Deslocamento: %d\n",
-               s->identificador.c_str(), s->tipo_v, s->nivel_lexico, s->deslocamento);
-    } else {
-        printf("Simbolo nullptr\n");
-    }
+  if (s != nullptr) {
+    printf("Simbolo: %s, Tipo: %d, Nivel Lexico: %d, Deslocamento: %d\n",
+           s->identificador.c_str(), s->tipo_v, s->nivel_lexico,
+           s->deslocamento);
+  } else {
+    printf("Simbolo nullptr\n");
+  }
 }
 
-void print_tabela(tabela_simbolos *tabela) {
-    if (is_stack_empty(tabela)) {
-        printf("┌─────────────────┐\n");
-        printf("│  Tabela Vazia!  │\n");
-        printf("└─────────────────┘\n");
-        return;
-    }
+void TabelaSimbolos::print_tabela() {
+  if (this->is_stack_empty()) {
+    printf("┌─────────────────┐\n");
+    printf("│  Tabela Vazia!  │\n");
+    printf("└─────────────────┘\n");
+    return;
+  }
 
-    t_node *current = tabela->top;
-    printf("┌──────────────────────────────────────────────────────────────┐\n");
-    printf("│                          Tabela de Símbolos                  │\n");
-    printf("├─────────────┬───────────┬───────────────┬────────────────────┤\n");
-    printf("│   Símbolo   │   Tipo    │ Nível Léxico  │    Deslocamento    │\n");
-    printf("├─────────────┼───────────┼───────────────┼────────────────────┤\n");
-    while (current != nullptr) {
-        printf("│ %-11s │ %-9s │ %-13d │ %-18d │\n",
-               current->simbolo->identificador.c_str(),
-               current->simbolo->tipo_v == t_undefined ? "Undefined" : (current->simbolo->tipo_v == t_int ? "Inteiro" : "Booleano"),
-               current->simbolo->nivel_lexico,
-               current->simbolo->deslocamento);
-        current = current->prev; // Mude para 'prev' para ir do topo ao fundo
-        if (current != nullptr) {
-            printf("├─────────────┼───────────┼───────────────┼────────────────────┤\n");
-        }
+  t_node *current = this->top;
+  printf("┌──────────────────────────────────────────────────────────────┐\n");
+  printf("│                          Tabela de Símbolos                  │\n");
+  printf("├─────────────┬───────────┬───────────────┬────────────────────┤\n");
+  printf("│   Símbolo   │   Tipo    │ Nível Léxico  │    Deslocamento    │\n");
+  printf("├─────────────┼───────────┼───────────────┼────────────────────┤\n");
+  while (current != nullptr) {
+    printf("│ %-11s │ %-9s │ %-13d │ %-18d │\n",
+           current->simbolo->identificador.c_str(),
+           current->simbolo->tipo_v == t_undefined
+               ? "Undefined"
+               : (current->simbolo->tipo_v == t_int ? "Inteiro" : "Booleano"),
+           current->simbolo->nivel_lexico, current->simbolo->deslocamento);
+    current = current->prev; // Mude para 'prev' para ir do topo ao fundo
+    if (current != nullptr) {
+      printf(
+          "├─────────────┼───────────┼───────────────┼────────────────────┤\n");
     }
-    printf("└─────────────┴───────────┴───────────────┴────────────────────┘\n");
+  }
+  printf("└─────────────┴───────────┴───────────────┴────────────────────┘\n");
 }
